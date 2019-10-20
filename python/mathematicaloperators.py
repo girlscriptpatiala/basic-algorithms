32bee43e55a984465895ccee4f62a861d5839498
dragonman164 <56438908+dragonman164@users.noreply.github.com>
2019-10-20 19:59:08 +0530

    Add files via upload

---
+++ python/mathematicaloperators.py
@@ -0,0 +1,11 @@
+#this file contains some of the very basic mathematical calculations
+a=int(input()) #input numbers
+b=int(input()) 
+print(a+b) #add two nos
+print(a-b) #subtract two nos
+print(a**b) #raise 5 to  the power two
+print(a//b) #5 divide by 2 with integer typecasting
+print(a*b) #multiply of 2 nos.
+print(a/b) #division of two nos with float typecasting
+print(a%b) #remainder of two nos. division
+
