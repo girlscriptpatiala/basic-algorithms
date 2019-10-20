9d0af015012f0bf75d11c8c22cdf6d8270674eae
dragonman164 <56438908+dragonman164@users.noreply.github.com>
2019-10-20 21:06:46 +0530

    Add files via upload

---
+++ C/mathematicaloperators.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+scanf("%d%d",a,b); //input of two numbers
+printf("%d\n",a+b); //sum of two numbers
+print("%d\n",a-b); //difference of two numbers
+printf("%d\n",a%b); //modulo i.e. remainder of division of two numbers
+printf("%d\n",a/b);  //type casting of division by int
+printf("%d\n",a*b); //multiplication of two nos.
+return 0;
+}
+
