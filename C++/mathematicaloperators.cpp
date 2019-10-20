be20051aa0e16dd6a76d15420a71b76994dd18dd
dragonman164 <56438908+dragonman164@users.noreply.github.com>
2019-10-20 20:54:34 +0530

    Add files via upload

---
+++ C++/mathematicaloperators.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+using namespace std;
+int main()
+{
+int a,b;
+cin>>a>>b; //input of two numbers
+cout<<a+b<<endl; //sum of two numbers
+cout<<a-b<<endl; //difference of two numbers
+cout<<a%b<<endl; //modulo i.e. remainder of division of two numbers
+cout<<float(a/b)<<endl; //type casting of division to float
+cout<<a/b;  //type casting of division by int
+cout<<a*b<<endl; //multiplication of two nos.
+return 0;
+}
+
